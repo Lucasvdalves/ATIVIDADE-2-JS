c9d6ce65da0b5937150064fcbee617e148ca4f0e
Lucas Damasceno <lucasalvesbh2018@gmail.com>
2023-06-06 22:16:31 -0300

    Atividade 1

---
+++ Exercicio - 1 - JS/script.js
@@ -0,0 +1,17 @@
+
+let numero1 = parseInt(prompt("Digite o primeiro número:"));
+let numero2 = parseInt(prompt("Digite o segundo número:"));
+let numero3 = parseInt(prompt("Digite o terceiro número:"));
+
+let mediaDosNumero = numero1 + numero2 + numero3
+
+let media = mediaDosNumero / 3
+
+alert("Seu resultado e : " + media)
+
+
+
+
+
+
+
