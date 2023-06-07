8dc812c62707cff1c88f04fd8414ce84b14f8c8f
Lucas Damasceno <lucasalvesbh2018@gmail.com>
2023-06-06 22:27:26 -0300

    Ativiadade 4

---
+++ Exercicio -3- JS/script.js
@@ -0,0 +1,6 @@
+let aluno = {}
+
+aluno.nome = prompt ("Digite o nome do aluno: ");
+aluno.idade = parseInt(prompt ("Digite a idade do aluno: "));
+
+alert("Voce buscou pelo aluno :" + aluno.nome  + " \n A idade dele e : " + aluno.idade);
