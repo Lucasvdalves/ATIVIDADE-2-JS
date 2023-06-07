34d37198cdf5fdc0f501e23da17e965f4b22a99a
Lucas Damasceno <lucasalvesbh2018@gmail.com>
2023-06-06 22:14:10 -0300

    Atividade 2

---
+++ Exercicio -2- JS/script.js
@@ -0,0 +1,13 @@
+alert("Ola seja bem vindo ao nosso sistema de calculo de IMC \n Por favor preencha todas as informacoes, sem constragimento para que possamos realizar o calculo exato e te fazer viver com mais saude \n =)")
+
+let peso = parseInt(prompt("Por favor informe o seu peso :"));
+let altura = parseInt(prompt("Agora informe a sua altura : "));
+
+let calculoIMC = peso / altura
+
+alert("Com base nas informacoes o seu IMC e de : " + calculoIMC)
+
+
+
+
+
